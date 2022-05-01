45d50070db27820fa2d6830536b9555fc56dfc01
Dmitriy Tverdokhlibov <dmitriytverdokhlibov@pop-os.localdomain>
2022-05-02 00:44:25 +0200

    refactor code

--- src/procedural/loops_iterators/App01.java
+++ src/procedural/loops_iterators/App01.java
@@ -2,19 +2,30 @@ package procedural.loops_iterators;
 
 import java.util.Arrays;
 
+/**
+ * @author dmitriy tverdokhlibov
+ */
 public class App01 {
     public static void main(String[] args) {
-        int[] array = new int[] {20, 30, 10, 50, 40};
-        for (int bar = array.length - 1; bar >= 0; bar--) {
-            for (int index = 0; index < array.length - 1; index++) {
-                if (array[index] > array[index + 1]) {
-                    int tmp = array[index];
-                    array[index] = array[index + 1];
-                    array[index + 1] = tmp;
-                }
+        int[] data = {20, 30, 10, 50, 40};
+        /** Bubble sort implementation by shifting large items to the right */
+        for (int bar = data.length - 1; bar >= 1; bar--) {
+            for (int index = 0; index < data.length - 1; index++) {
+                ConditionalSwap(data, index);
             }
         }
+        System.out.println(Arrays.toString(data));
+    }
 
-        System.out.println(Arrays.toString(array));
+    private static void ConditionalSwap(int[] array, int index) {
+        if (array[index] > array[index + 1]) {
+            swap(array, index);
+        }
+    }
+
+    private static void swap(int[] array, int index) {
+        int tmp = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = tmp;
     }
 }
