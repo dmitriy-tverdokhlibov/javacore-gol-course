2b45d64223f2b7a01520b5015a74206b16465dc3
Dmitriy Tverdokhlibov <dmitriytverdokhlibov@pop-os.localdomain>
2022-05-08 18:36:41 +0200

    added SelectionSort class and IsertionSort class

---
+++ src/procedural/SelectionSort.java
@@ -0,0 +1,36 @@
+package procedural;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] data = {20, 30, 10, 50, 40};
+        sort(data);
+        System.out.println(Arrays.toString(data));
+    }
+
+    public static void sort(int[] arr) {
+        for (int barrier = 0; barrier < arr.length - 1; barrier++) {
+            for (int index = barrier + 1; index < arr.length; index++) {
+                if (arr[barrier] > arr[index]) {
+                    int tmp = arr[index];
+                    arr[index] = arr[barrier];
+                    arr[barrier] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void sortOptimized(int[] arr) {
+        for (int barrier = 0; barrier < arr.length - 1; barrier++) {
+            int element = arr[barrier];
+            for (int index = barrier + 1; index < arr.length; index++) {
+                if (element > arr[index]) {
+                    int tmp = arr[index];
+                    arr[index] = arr[barrier];
+                    arr[barrier] = tmp;
+                }
+            }
+        }
+    }
+}
---
+++ src/procedural/loops_iterators/InsertionSort.java
@@ -0,0 +1,19 @@
+package procedural.loops_iterators;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    //TODO Insertion sort
+
+    public static void sort(int[] arr) {
+        for (int k = 1; k < arr.length; k++) {
+            int newElement = arr[k];
+            int location = k - 1;
+            while (location <= 0 && arr[location] > newElement) {
+                arr[location + 1] = arr[location];
+                location--;
+            }
+            arr[location + 1] = newElement;
+        }
+    }
+}
